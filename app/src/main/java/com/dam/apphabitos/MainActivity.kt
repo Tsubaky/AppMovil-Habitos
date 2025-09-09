f1a6ab3831ff1f890657b4418ad79316b8f6bbe3
skulltom666 <skulltom666@outlook.com>
2025-09-09 16:19:58 -0500

    cambio de nombre

--- app/src/main/java/com/dam/apphabitos/MainActivity.kt
+++ app/src/main/java/com/dam/apphabitos/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppHabitosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Proyecto",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
