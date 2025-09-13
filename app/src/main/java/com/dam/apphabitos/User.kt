3f20ca1fb1ad3ea51d0ba4563829bdf76b300a7a
skulltom666 <skulltom666@outlook.com>
2025-09-13 11:24:36 -0500

    v2 del registro

--- app/src/main/java/com/dam/apphabitos/User.kt
+++
@@ -1,9 +0,0 @@
-package com.dam.apphabitos
-
-data class User(
-    val id: String = "",
-    val fullName: String = "",
-    val email: String = "",
-    val phone: String = "",
-    val createdAt: Long = System.currentTimeMillis()
-)
